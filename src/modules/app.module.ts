e1bfdca3d642cb1692b3146736a4f92390625a02
Mark Castellà Schenzle <markseisler@gmail.com>
2018-10-01 09:25:51 +0200

    first commit

---
+++ src/modules/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule {}
